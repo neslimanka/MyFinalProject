504e5130c78a0711117162603fcaeef4ddf76cb6
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-08-18 22:07:24 +0300

    web api

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            { 
+                new Product { ProductId = 3, ProductName = "Elma" },
+                new Product { ProductId = 4, ProductName = "Armut" }
+            };
+    }
+     
+            
+    }
+}
