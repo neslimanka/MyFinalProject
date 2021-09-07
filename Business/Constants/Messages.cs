fadbf8edd12cde60193e7d2b263f3ac5f618ab84
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-08 01:19:55 +0300

    JWT entegrasyonu yapıldı

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -12,5 +14,12 @@ namespace Business.Constants
         public static string ProductsListed = "Ürünler listelendi";
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists = "Böyle bir isimde ürün zaten var";
+        public  static string AuthorizationDenied="Erişim Hatası";
+        public static string UserRegistered="Kullanıcı kayıtlı";
+        public static string UserNotFound="Kullanıcı bulunamadı";
+        public static string PasswordError="Parola hatalı";
+        public static string SuccessfulLogin="Başarılı Giriş";
+        public static string UserAlreadyExists="Kullancı zaten var";
+        public static string AccessTokenCreated="Token oluşturuldu";
     }
 }
